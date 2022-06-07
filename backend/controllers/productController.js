af3f5be1a597c273acec200bb5a514eac104e8d8
Devesh225 <deveshtulshyan@gmail.com>
2022-06-07 19:28:27 +0530

    Product Routes and Controllers complete, API's tested with Postman

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,16 @@
 const productModel = require('../models/productModel');
 
 
+// GET ALL PRODUCTS
+exports.getAllProducts = async(req, res) => {
+    const products = await productModel.find();
+    res.status(200).json({
+        success: true,
+        products
+    });
+}
+
+
 // CREATE A PRODUCT
 exports.createProduct = async(req, res) => {
     const product = await productModel.create(req.body);
@@ -10,8 +20,53 @@ exports.createProduct = async(req, res) => {
     });
 }
 
-
-// GET ALL PRODUCTS
-exports.getAllProducts = (req, res) => {
-    res.status(200).json({message: "Route is working fine."});
+// UPDATE PRODUCT
+exports.updateProduct = async(req, res) => {
+    let product = await productModel.findById(req.params.id);
+    if(!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product not found."
+        });
+    }
+    
+    product = await productModel.findByIdAndUpdate(req.params.id, req.body, {new:true, runValidators:true});
+    res.status(200).json({
+        success: true,
+        product
+    });
 }
+
+// DELETE PRODUCT
+exports.deleteProduct = async(req, res) => {
+    const product = await productModel.findById(req.params.id);
+    if(!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product not found."
+        });
+    }
+    await product.remove();
+    res.status(200).json({
+        success: true,
+        message: "Product Deleted Successfully."
+    });
+}
+
+// GET A SINGLE PRODUCT / PRODUCT DETAILS
+exports.getProduct = async(req, res) => {
+    const product = await productModel.findById(req.params.id);
+    if(!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product not found."
+        });
+    }
+
+    res.status(200).json({
+        success: true,
+        product
+    });
+
+}
+
