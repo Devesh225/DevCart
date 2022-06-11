ae570a7688d1266c84c4437f3cf0fd7cdedb0dd8
Devesh225 <deveshtulshyan@gmail.com>
2022-06-11 14:56:50 +0530

    Password hashing and JWT token

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -38,6 +40,28 @@ const userSchema = new mongoose.Schema({
     resetPasswordExpire: Date,
 
 });
+
+// BEFORE SAVING THE DOCUMENT IN THE COLLECTION :- pre(save) BEFORE SAVING
+
+userSchema.pre("save", async function(next) {
+
+    // IF PASSWORD IS MODIFIED, THEN ONLY WE WANT IT TO BE HASHED, BECAUSE IF IT IS NOT MODIFIED
+    // IT IS ALREADY HASHED, SO IT WILL BE HASHED AGAIN, WHICH IS NOT OPTIMAL.
+    if(!this.isModified("password")) {
+        next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 10); // 10 is the SALTING POWER
+});
+
+// JWT TOKEN IMPLEMENTATION, GENERATE TOKEN AND STORE IN THE COOKIE.
+// JWT_SECRET is the secret key which can be used to access the login of the user.
+userSchema.methods.getJwtToken = function() {
+    return jwt.sign({id: this._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_TOKEN_EXPIRE
+    });
+}
+
 
 const userModel = mongoose.model("User", userSchema);
 
