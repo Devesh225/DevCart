d181c2aa007c7a28c077de29372b22813077102c
Devesh225 <deveshtulshyan@gmail.com>
2022-06-14 14:47:15 +0530

    Duplicate Key Error Handled

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -11,6 +11,12 @@ const checkError = (err, req, res, next) => {
         err = new ErrorHandler(message, 500);
     }
 
+    // MongoDB Duplicate Key Error
+    if(err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered `;
+        err = new ErrorHandler(message, 500);
+    }
+
     res.status(err.statusCode).json({
         success: false,
         message: err.message
