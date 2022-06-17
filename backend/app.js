54c317d785fe6cf63500574a875428e4289113e9
Devesh225 <deveshtulshyan@gmail.com>
2022-06-17 21:43:09 +0530

    added middleware for order route api's in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const productRoutes = require('./routes/productRoute');
 const userRoutes = require('./routes/userRoute');
+const orderRoutes = require('./routes/orderRoute');
 const checkError = require('./middlewares/error');
 const cookieParser = require('cookie-parser');
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api", productRoutes);
 app.use("/api", userRoutes);
+app.use("/api", orderRoutes)
 
 // ERROR MIDDLEWARE
 app.use(checkError);
