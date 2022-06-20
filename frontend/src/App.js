93d184834072004ba64e5556d3690a7e2f6e6792
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-06-20 21:03:20 +0530

    header component UI, and styling complete, used webfontloader for fonts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
 import Header from './components/layout/Header.js';
 import { BrowserRouter as Router } from  'react-router-dom'; 
+import WebFont from 'webfontloader';
+import { useEffect } from 'react';
 
 function App() {
+
+// TO LOAD THE FONT at componentDidMount
+useEffect(() => {
+  WebFont.load({
+    google: {
+      families: ["Roboto", "Droid Sans"]
+    }
+  });
+}, []);
+
+
   return (
     <Router>
       <Header />
