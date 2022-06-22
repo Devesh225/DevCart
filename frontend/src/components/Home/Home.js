b8790c1b6c8cfdf5ed0885de52492e93e0d5ba21
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-06-22 15:52:02 +0530

    home section layout and UI changes

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,6 +1,17 @@
 import React, { Fragment } from 'react'
 import { CgMouse } from  'react-icons/cg';
+import Product from './Product.js'
 import './Home.css'
+
+// SAMPLE PRODUCT USED FOR TESTING BEFORE REDUX IMPLEMENTATION
+const product = {
+  _id: "jwavavapgvpava",
+  name: "Apple iPhone X",
+  price: 150000,
+  images: [{
+    url: "sample URL"
+  }]
+};
 
 const Home = () => {
   return (
@@ -14,8 +25,11 @@ const Home = () => {
                 Scroll <CgMouse />
               </button>
             </a>
+        </div>
+        <h2 className="homeHeading">FEATURED PRODUCTS</h2>
 
-            <h2 className="homeHeading">Featured Products</h2>
+        <div className="container" id="container">
+          <Product product={product} />
         </div>
     </Fragment>
   )
