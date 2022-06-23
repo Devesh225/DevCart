2bf87a2c746028e3ffa857137dfe5350486eb151
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-06-23 21:46:19 +0530

    reducer folder, and product reducer created, basic implementation complete

---
+++ frontend/src/reducers/productReducer.js
@@ -0,0 +1,36 @@
+// We need to pass a state and an action, state is an empty list of products.
+// Importing Constants 
+import { ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_FAILURE, CLEAR_ERRORS } from '../constants/productConstants';
+
+
+export const productReducer = (state = {products: []}, action) => {
+    switch (action.type) {
+        case ALL_PRODUCT_REQUEST:
+            return {
+                loading: true,
+                product: []
+            }
+
+        case ALL_PRODUCT_SUCCESS:
+            return {
+                loading: false,
+                product: action.payload.products,
+                productCount: action.payload.productCount
+            }
+        
+        case ALL_PRODUCT_FAILURE:
+            return {
+                loading: false,
+                error: action.payload
+            }
+
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+    
+        default:
+            return state;
+    }
+}
