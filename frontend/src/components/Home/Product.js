8f682cd668a89d9428ddf1d90a5d042994e4e0ac
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-06-24 21:56:36 +0530

    fetching data from redux state

--- frontend/src/components/Home/Product.js
+++ frontend/src/components/Home/Product.js
@@ -3,22 +3,23 @@ import { Link } from 'react-router-dom';
 import ReactStars from 'react-rating-stars-component';
 import './Product.css';
 
-const options = {
+const Product = ({ product }) => {
+
+  const options = {
     edit: false, // CANNOT EDIT THE STARS
     color: "#393E46",
     activeColor: "#00ADB5",
-    value: 3.5,
+    value: product.rating,
     isHalf: true, // IF IT IS NOT GIVEN, RATINGS WILL BE SHOWN IN INTEGER VALUES, SO 3.5 WOULD BE SHOWN AS 3
     size: window.innerWidth < 600 ? 15 : 20
-}
+  }
 
-const Product = ({ product }) => {
   return (
     <Link className="productCard" to={product._id}>
         <img src={product.images[0].url} alt={product.name} />
         <p>{product.name}</p>
         <div>
-            <ReactStars {...options} /> <span>(28 Reviews)</span>
+            <ReactStars {...options} /> <span>({product.numberOfReviews} Reviews)</span>
         </div>
         <span>{product.price}</span>
     </Link>
