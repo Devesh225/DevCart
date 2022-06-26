5dfe9e577f9f32f0af03ace7033ca3d7cd6ebbb1
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-06-26 16:32:52 +0530

    created reducer for single product details

--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -1,9 +1,9 @@
 // We need to pass a state and an action, state is an empty list of products.
 // Importing Constants 
-import { ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_FAILURE, CLEAR_ERRORS } from '../constants/productConstants';
+import { ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_FAILURE, CLEAR_ERRORS, PRODUCT_REQUEST, PRODUCT_SUCCESS, PRODUCT_FAILURE } from '../constants/productConstants';
 
 
-export const productReducer = (state = {products: []}, action) => {
+export const allProductsReducer = (state = {products: []}, action) => {
     switch (action.type) {
         case ALL_PRODUCT_REQUEST:
             return {
@@ -15,10 +15,41 @@ export const productReducer = (state = {products: []}, action) => {
             return {
                 loading: false,
                 products: action.payload.data.products,
-                productCount: action.payload.data.productCount
+                productsCount: action.payload.data.productCount
             }
         
         case ALL_PRODUCT_FAILURE:
+            return {
+                loading: false,
+                error: action.payload
+            }
+
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+    
+        default:
+            return state;
+    }
+}
+
+export const productReducer = (state = {product: {}}, action) => {
+    switch (action.type) {
+        case PRODUCT_REQUEST:
+            return {
+                loading: true,
+                ...state
+            }
+
+        case PRODUCT_SUCCESS:
+            return {
+                loading: false,
+                product: action.payload.data.product,
+            }
+        
+        case PRODUCT_FAILURE:
             return {
                 loading: false,
                 error: action.payload
