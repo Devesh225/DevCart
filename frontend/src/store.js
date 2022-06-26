77703787a6bd0e4f9b5ea8a7f05ad31a8dadc1ed
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-06-26 16:31:22 +0530

    added get single product details reducer in store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,19 +1,18 @@
 // NECESSARY IMPORTS
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { legacy_createStore as createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productReducer } from './reducers/productReducer';
+import { allProductsReducer, productReducer } from './reducers/productReducer';
 
-const reducer = combineReducers({
-    products: productReducer
+const reducers = combineReducers({
+    products: allProductsReducer,
+    product: productReducer,
 });
 
-let initialState = {
-
-};
+let initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+const store = createStore(reducers, initialState, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
