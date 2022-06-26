1feb8446f994dc4615ce38f3d425d50c6bfa6e5b
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-06-26 14:18:44 +0530

    react alert error template added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store';
+import { positions, transitions, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
 import App from './App';
+
+const options = {
+  timeout: 5000, // 5 seconds,
+  position: positions.TOP_CENTER,
+  transition: transitions.SCALE
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <AlertProvider template={AlertTemplate} {...options} >
+      <App />
+    </AlertProvider>
   </Provider>
 );
 
