d4e295fdc8c52a17cb9ccc581e828a0bd94f5407
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-06-26 16:33:09 +0530

    created action for single product details

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import { ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_FAILURE, CLEAR_ERRORS } from '../constants/productConstants';
+import { ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_FAILURE, CLEAR_ERRORS, PRODUCT_REQUEST, PRODUCT_SUCCESS, PRODUCT_FAILURE } from '../constants/productConstants';
 
-export const getProduct = () => async (dispatch) => {
+export const getAllProducts = () => async (dispatch) => {
     try {
         dispatch({
             type: ALL_PRODUCT_REQUEST,
@@ -15,6 +15,25 @@ export const getProduct = () => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: ALL_PRODUCT_FAILURE,
+            payload: error.response.data.message
+        });
+    }
+}
+
+export const getProduct = (id) => async (dispatch) => {
+    try {
+        dispatch({
+            type: PRODUCT_REQUEST,
+        });
+        const data = await axios.get(`/api/product/${id}`);
+
+        dispatch({
+            type: PRODUCT_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_FAILURE,
             payload: error.response.data.message
         });
     }
