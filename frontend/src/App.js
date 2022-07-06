d79c2bf099fc63d4ff2a6e0d48894453aef48587
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-07-06 23:26:21 +0530

    search route added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,6 +30,7 @@ useEffect(() => {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/product/:id" element={<ProductDetails />} />
         <Route exact path="/products" element={<Products />} />
+        <Route path="/products/:keyword" element={<Products />} />
         <Route exact path="/search" element={<Search />} />
       </Routes>
       <Footer />
