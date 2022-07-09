6b131aa374ac9e9d6ea03966df5da5737cafbe2f
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-07-10 01:02:28 +0530

    implemented pagination

--- frontend/src/components/Product/Products.js
+++ frontend/src/components/Product/Products.js
@@ -1,20 +1,27 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import './Products.css'
 import { useDispatch, useSelector } from 'react-redux';
 import Loading from '../layout/Loading/Loading';
 import { getAllProducts } from '../../actions/productAction';
 import Product from '../Home/Product';
 import { useParams } from 'react-router-dom';
+import Pagination from 'react-js-pagination';
 
 
 const Products = () => {
     const dispatch = useDispatch();
     const { keyword } = useParams();
-    const { loading, error, products, productsCount } = useSelector(state => state.allProducts);
+    const { loading, error, products, productsCount, resultPerPage } = useSelector(state => state.allProducts);
+
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const setCurrentPageNumber = (e) => {
+      setCurrentPage(e);
+    }
 
     useEffect(() => {
-      dispatch(getAllProducts(keyword));
-    }, [dispatch, keyword])
+      dispatch(getAllProducts(keyword, currentPage));
+    }, [dispatch, keyword, currentPage])
     
   return (
     <Fragment>
@@ -25,6 +32,27 @@ const Products = () => {
                     return <Product key={index} product={product} />
                 })}
             </div>
+
+            {/* ONLY SHOW PAGINATION WHEN RESULTS PER PAGE IS LESS THAN PDOCUTS COUNT. */}
+            { resultPerPage < productsCount && 
+              <div className="paginationBox">
+                <Pagination
+                  activePage={currentPage}
+                  itemsCountPerPage={resultPerPage}
+                  totalItemsCount={productsCount}
+                  onChange={setCurrentPageNumber}
+                  nextPageText="Next"
+                  prevPageText="Prev"
+                  firstPageText="First"
+                  lastPageText="Last"
+                  itemClass="page-item"
+                  linkClass="page-link"
+                  activeClass="pageItemActive"
+                  activeLinkClass="pageLinkActive"
+                  pageRangeDisplayed="3"
+                />
+              </div>
+            }
         </Fragment>) }
     </Fragment>
   );
