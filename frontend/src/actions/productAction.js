1eb86f33c40ca4662940549f620828706a89fa14
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-07-10 17:27:33 +0530

    resolved bug of products not rendering on home page, and implemented filters

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -1,14 +1,20 @@
 import axios from 'axios';
 import { ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_FAILURE, CLEAR_ERRORS, PRODUCT_REQUEST, PRODUCT_SUCCESS, PRODUCT_FAILURE } from '../constants/productConstants';
 
-export const getAllProducts = (keyword="", currentPage=1, price=[0, 500000]) => async (dispatch) => {
+export const getAllProducts = (keyword="", currentPage=1, price=[0, 500000], category="", rating=0) => async (dispatch) => {
     try {
         dispatch({
             type: ALL_PRODUCT_REQUEST,
         });
 
-        const data = await axios.get(`/api/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}`);
-        
+        let link = `/api/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&rating[gte]=${rating}`;
+
+        if(category) {
+            link = link + `&category=${category}`
+        }
+
+        const data = await axios.get(link);
+
         dispatch({
             type: ALL_PRODUCT_SUCCESS,
             payload: data
