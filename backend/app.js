e35710619bcc5ea2dd58e843be6c30a8f617a646
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-07-16 18:33:39 +0530

    body parser and file upload setup

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,16 @@ const userRoutes = require('./routes/userRoute');
 const orderRoutes = require('./routes/orderRoute');
 const checkError = require('./middlewares/error');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+
+
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(fileUpload());
 app.use("/api", productRoutes);
 app.use("/api", userRoutes);
 app.use("/api", orderRoutes)
