c4781910dfbe46eafcb7e44121a0a7166d4cc5b3
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-07-16 18:49:03 +0530

    implemented load logged in user functionality

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, CLEAR_ERRORS, REGISTER_REQUEST, REGISTER_SUCCESS, REGISTER_FAILURE} from '../constants/userConstants';
+import {LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, CLEAR_ERRORS, REGISTER_REQUEST, REGISTER_SUCCESS, REGISTER_FAILURE, LOAD_LOGGED_IN_USER_REQUEST, LOAD_LOGGED_IN_USER_SUCCESS, LOAD_LOGGED_IN_USER_FAILURE} from '../constants/userConstants';
 
 export const login = (email, password) => async(dispatch) => {
     try {
@@ -65,8 +65,30 @@ export const register = (userFormData) => async(dispatch) => {
     }
 };
 
+export const loadLoggedInUser = () => async(dispatch) => {
+    try {
+        dispatch({
+            type: LOAD_LOGGED_IN_USER_REQUEST
+        });
+
+        const data = await axios.get(`/api/me`,);
+
+        dispatch({
+            type: LOAD_LOGGED_IN_USER_SUCCESS,
+            payload: data 
+        })
+
+
+    } catch (error) {
+        dispatch({
+            type: LOAD_LOGGED_IN_USER_FAILURE,
+            payload: error.response.data.message
+        });
+    }
+};
+
 export const clearErrors = () => async(dispatch) => {
     dispatch({
         type: CLEAR_ERRORS
     });
-}
+};
