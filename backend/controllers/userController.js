35ac3b0a4229713de3c1248eb04c10ab22157504
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-07-16 18:34:01 +0530

    cloudinary implemented for avatar upload and storage of users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,17 +4,25 @@ const userModel = require('../models/userModel');
 const sendToken = require('../utils/jwtToken');
 const sendEmail = require(`../utils/sendEmail`);
 const crypto = require('crypto');
+const cloudinary = require('cloudinary');
 
 // REGISTER A USER FUNCTION
 exports.registerUser = catchAsyncError(async(req, res, next) => {
+
+    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+        folder: "avatars",
+        width: 150,
+        crop: "scale"
+    });
+
     const {name, email, password} = req.body;
     const user = await userModel.create({
         name, 
         email, 
         password,
         avatar: {
-            public_id: "This is a Sample Public ID",
-            url: "This is a Sample URL",
+            public_id: myCloud.public_id,
+            url: myCloud.secure_url,
         }
     });
 
