983cf15a49fd09d185bcf9c9318efdd9682d32fd
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-07-16 18:49:20 +0530

    implemented load logged in user reducer

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -1,15 +1,17 @@
-import { CLEAR_ERRORS, LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, REGISTER_FAILURE, REGISTER_REQUEST, REGISTER_SUCCESS } from "../constants/userConstants";
+import { CLEAR_ERRORS, LOAD_LOGGED_IN_USER_FAILURE, LOAD_LOGGED_IN_USER_REQUEST, LOAD_LOGGED_IN_USER_SUCCESS, LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, REGISTER_FAILURE, REGISTER_REQUEST, REGISTER_SUCCESS } from "../constants/userConstants";
 
 export const userReducer = (state = {user: {}}, action) => {
     switch (action.type) {
         case LOGIN_REQUEST:
         case REGISTER_REQUEST:
+        case LOAD_LOGGED_IN_USER_REQUEST:
             return {
                 loading: true,
                 isAuthenticated: false
             };
         case LOGIN_SUCCESS: 
         case REGISTER_SUCCESS:
+        case LOAD_LOGGED_IN_USER_SUCCESS:
             return {
                 ...state,
                 loading: false,
@@ -20,6 +22,13 @@ export const userReducer = (state = {user: {}}, action) => {
         case REGISTER_FAILURE:
             return {
                 ...state,
+                loading: false,
+                isAuthenticated: false,
+                user: null,
+                error: action.payload
+            }
+        case LOAD_LOGGED_IN_USER_FAILURE:
+            return {
                 loading: false,
                 isAuthenticated: false,
                 user: null,
