751dcd55f3665e3a24c69ed70ee5868f4ebe2901
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-07-16 18:05:50 +0530

    user register implementation

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, CLEAR_ERRORS} from '../constants/userConstants';
+import {LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, CLEAR_ERRORS, REGISTER_REQUEST, REGISTER_SUCCESS, REGISTER_FAILURE} from '../constants/userConstants';
 
 export const login = (email, password) => async(dispatch) => {
     try {
@@ -21,13 +21,45 @@ export const login = (email, password) => async(dispatch) => {
 
         dispatch({
             type: LOGIN_SUCCESS,
-            payload: data.user 
+            payload: data 
         })
 
 
     } catch (error) {
         dispatch({
             type: LOGIN_FAILURE,
+            payload: error.response.data.message
+        });
+    }
+};
+
+export const register = (userFormData) => async(dispatch) => {
+    try {
+        dispatch({
+            type: REGISTER_REQUEST
+        });
+
+        const config = {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }
+
+        const data = await axios.post(
+            `/api/register`,
+            userFormData,
+            config
+        );
+
+        dispatch({
+            type: REGISTER_SUCCESS,
+            payload: data 
+        })
+
+
+    } catch (error) {
+        dispatch({
+            type: REGISTER_FAILURE,
             payload: error.response.data.message
         });
     }
