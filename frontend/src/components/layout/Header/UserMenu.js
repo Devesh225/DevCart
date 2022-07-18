cabf50107abb27d34dc10dd996739812d8c4d441
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-07-18 22:34:22 +0530

    created user options menu for shortcut navigation

--- frontend/src/components/layout/Header/UserMenu.js
+++ frontend/src/components/layout/Header/UserMenu.js
@@ -1,16 +1,20 @@
 import React, { Fragment, useState } from 'react'
-import { SpeedDial, SpeedDialAction } from '@mui/material';
+import { SpeedDial, SpeedDialAction, Backdrop } from '@mui/material';
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import PersonIcon from "@mui/icons-material/Person";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import ListAltIcon from "@mui/icons-material/ListAlt";
 import { useAlert } from 'react-alert';
 import { useNavigate } from 'react-router-dom';
+import { logout } from '../../../actions/userAction';
+import { useDispatch } from 'react-redux';
+import './UserMenu.css';
 
 const UserMenu = ({ user }) => {
     const [open, setOpen] = useState(false);
     const alert = useAlert();
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const options = [
         { icon: <ListAltIcon />, name: "Orders", func: orders },
@@ -39,24 +43,26 @@ const UserMenu = ({ user }) => {
     }
 
     function logoutUser() {
-        // dispatch(logout());
+        dispatch(logout());
         alert.success("Logout Successfully");
     }
 
   return (
     <Fragment>
+      <Backdrop open={open} style={{zIndex: "1"}} />
       <SpeedDial
         ariaLabel="SpeedDial"
         onClose={() => setOpen(false)}
         onOpen={() => setOpen(true)}
         open={open}
+        style={{zIndex: "2"}}
         direction="down"
         className="speedDial"
         icon={
           <img
             className="speedDialIcon"
-            src={user.avatar.url ? user.avatar.url : "/avatarPreview.png"}
-            alt="Profile"
+            src={user.avatar.url ? user.avatar.url : "./avatarPreview.png"}
+            alt=""
           />
         }
       >
