4695344675ee6de5bf3c8a2876f201f03787981a
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-07-24 21:02:44 +0530

    actions for update profile created

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, CLEAR_ERRORS, REGISTER_REQUEST, REGISTER_SUCCESS, REGISTER_FAILURE, LOAD_LOGGED_IN_USER_REQUEST, LOAD_LOGGED_IN_USER_SUCCESS, LOAD_LOGGED_IN_USER_FAILURE, LOGOUT_SUCCESS, LOGOUT_FAILURE} from '../constants/userConstants';
+import {LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, CLEAR_ERRORS, REGISTER_REQUEST, REGISTER_SUCCESS, REGISTER_FAILURE, LOAD_LOGGED_IN_USER_REQUEST, LOAD_LOGGED_IN_USER_SUCCESS, LOAD_LOGGED_IN_USER_FAILURE, LOGOUT_SUCCESS, LOGOUT_FAILURE, UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_SUCCESS, UPDATE_PROFILE_FAILURE} from '../constants/userConstants';
 
 export const login = (email, password) => async(dispatch) => {
     try {
@@ -100,6 +100,38 @@ export const logout = () => async(dispatch) => {
     } catch (error) {
         dispatch({
             type: LOGOUT_FAILURE,
+            payload: error.response.data.message
+        });
+    }
+};
+
+export const updateProfile = (userFormData) => async(dispatch) => {
+    try {
+        dispatch({
+            type: UPDATE_PROFILE_REQUEST
+        });
+
+        const config = {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }
+
+        const data = await axios.put(
+            `/api/me/update`,
+            userFormData,
+            config
+        );
+
+        dispatch({
+            type: UPDATE_PROFILE_SUCCESS,
+            payload: data 
+        })
+
+
+    } catch (error) {
+        dispatch({
+            type: UPDATE_PROFILE_FAILURE,
             payload: error.response.data.message
         });
     }
