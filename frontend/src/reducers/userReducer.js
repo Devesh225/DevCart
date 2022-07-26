598cb7751c63326ee056d46082f956dc2fc98e57
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-07-26 20:28:10 +0530

    implemented reducer for update password

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -1,4 +1,4 @@
-import { CLEAR_ERRORS, LOAD_LOGGED_IN_USER_FAILURE, LOAD_LOGGED_IN_USER_REQUEST, LOAD_LOGGED_IN_USER_SUCCESS, LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT_FAILURE, LOGOUT_SUCCESS, REGISTER_FAILURE, REGISTER_REQUEST, REGISTER_SUCCESS, UPDATE_PROFILE_FAILURE, UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_RESET, UPDATE_PROFILE_SUCCESS } from "../constants/userConstants";
+import { CLEAR_ERRORS, LOAD_LOGGED_IN_USER_FAILURE, LOAD_LOGGED_IN_USER_REQUEST, LOAD_LOGGED_IN_USER_SUCCESS, LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT_FAILURE, LOGOUT_SUCCESS, REGISTER_FAILURE, REGISTER_REQUEST, REGISTER_SUCCESS, UPDATE_PASSWORD_FAILURE, UPDATE_PASSWORD_REQUEST, UPDATE_PASSWORD_RESET, UPDATE_PASSWORD_SUCCESS, UPDATE_PROFILE_FAILURE, UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_RESET, UPDATE_PROFILE_SUCCESS } from "../constants/userConstants";
 
 export const userReducer = (state = {user: {}}, action) => {
     switch (action.type) {
@@ -59,23 +59,27 @@ export const userReducer = (state = {user: {}}, action) => {
 export const updateProfileReducer = (state = {}, action) => {
     switch (action.type) {
         case UPDATE_PROFILE_REQUEST:
+        case UPDATE_PASSWORD_REQUEST:
             return {
                 ...state,
                 loading: true,
             };
         case UPDATE_PROFILE_SUCCESS: 
+        case UPDATE_PASSWORD_SUCCESS:
             return {
                 ...state,
                 loading: false,
                 isUpdated: action.payload
             }
         case UPDATE_PROFILE_FAILURE:
+        case UPDATE_PASSWORD_FAILURE:
             return {
                 ...state,
                 loading: false,
                 error: action.payload
             }
         case UPDATE_PROFILE_RESET:
+        case UPDATE_PASSWORD_RESET:
             return {
                 ...state,
                 isUpdated: false
