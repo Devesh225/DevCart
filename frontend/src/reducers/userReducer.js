115a941b336c22f207fb37f9b0bc57ccc4ef5ce6
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-08-02 23:44:09 +0530

    reset password reducer created

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -1,4 +1,4 @@
-import { CLEAR_ERRORS, FORGOT_PASSWORD_FAILURE, FORGOT_PASSWORD_REQUEST, FORGOT_PASSWORD_SUCCESS, LOAD_LOGGED_IN_USER_FAILURE, LOAD_LOGGED_IN_USER_REQUEST, LOAD_LOGGED_IN_USER_SUCCESS, LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT_FAILURE, LOGOUT_SUCCESS, REGISTER_FAILURE, REGISTER_REQUEST, REGISTER_SUCCESS, UPDATE_PASSWORD_FAILURE, UPDATE_PASSWORD_REQUEST, UPDATE_PASSWORD_RESET, UPDATE_PASSWORD_SUCCESS, UPDATE_PROFILE_FAILURE, UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_RESET, UPDATE_PROFILE_SUCCESS } from "../constants/userConstants";
+import { CLEAR_ERRORS, FORGOT_PASSWORD_FAILURE, FORGOT_PASSWORD_REQUEST, FORGOT_PASSWORD_SUCCESS, LOAD_LOGGED_IN_USER_FAILURE, LOAD_LOGGED_IN_USER_REQUEST, LOAD_LOGGED_IN_USER_SUCCESS, LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT_FAILURE, LOGOUT_SUCCESS, REGISTER_FAILURE, REGISTER_REQUEST, REGISTER_SUCCESS, RESET_PASSWORD_FAILURE, RESET_PASSWORD_REQUEST, RESET_PASSWORD_SUCCESS, UPDATE_PASSWORD_FAILURE, UPDATE_PASSWORD_REQUEST, UPDATE_PASSWORD_RESET, UPDATE_PASSWORD_SUCCESS, UPDATE_PROFILE_FAILURE, UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_RESET, UPDATE_PROFILE_SUCCESS } from "../constants/userConstants";
 
 export const userReducer = (state = {user: {}}, action) => {
     switch (action.type) {
@@ -97,6 +97,7 @@ export const updateProfileReducer = (state = {}, action) => {
 export const forgotPasswordReducer = (state = {}, action) => {
     switch (action.type) {
         case FORGOT_PASSWORD_REQUEST:
+        case RESET_PASSWORD_REQUEST:
             return {
                 ...state,
                 loading: true,
@@ -108,7 +109,14 @@ export const forgotPasswordReducer = (state = {}, action) => {
                 loading: false,
                 message: action.payload
             }
+        case RESET_PASSWORD_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                success: action.payload
+            }
         case FORGOT_PASSWORD_FAILURE:
+        case RESET_PASSWORD_FAILURE:
             return {
                 ...state,
                 loading: false,
