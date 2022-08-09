f4d6d982dc6c97faed21f341c9cb986165bfd0cb
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-08-09 22:03:15 +0530

    implemented reducer for cart state and shipping info state

--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART } from "../constants/cartConstants";
+import { ADD_TO_CART, REMOVE_FROM_CART, SAVE_SHIPPING_INFO } from "../constants/cartConstants";
 
 export const cartReducer = (
     state = { cartItems: [], shippingInfo: {} },
@@ -24,6 +24,17 @@ export const cartReducer = (
             ...state,
             cartItems: [...state.cartItems, item],
           };
+        }
+
+      case REMOVE_FROM_CART:
+        return {
+          ...state,
+          cartItems: state.cartItems.filter((item) => item.product !== action.payload)
+        };
+      case SAVE_SHIPPING_INFO:
+        return {
+          ...state,
+          shippingInfo: action.payload
         }
     default:
         return state
