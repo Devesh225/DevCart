a7ed805a021973b708883d3ca29fafc99aa0582b
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-08-09 22:04:03 +0530

    created actions for shipping information

--- frontend/src/actions/cartAction.js
+++ frontend/src/actions/cartAction.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { ADD_TO_CART } from '../constants/cartConstants';
+import { ADD_TO_CART, REMOVE_FROM_CART, SAVE_SHIPPING_INFO } from '../constants/cartConstants';
 
 export const addItemsToCart = (id, quantity) => async (dispatch, getState) => {
     const { data } = await axios.get(`/api/product/${id}`);
@@ -18,3 +18,21 @@ export const addItemsToCart = (id, quantity) => async (dispatch, getState) => {
     // SAVING CART DETAILS IN LOCAL STORAGE
     localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
+
+export const removeItemsFromCart = (id) => async(dispatch, getState) => {
+    dispatch({
+        type: REMOVE_FROM_CART,
+        payload: id
+    });
+
+    localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+}
+
+export const saveShippingInfo = (data) => async(dispatch) => {
+    dispatch({
+        type: SAVE_SHIPPING_INFO,
+        payload: data
+    });
+
+    localStorage.setItem("shippingInfo", JSON.stringify(data));
+}
