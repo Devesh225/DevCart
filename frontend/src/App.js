66a110fc341b312313e4f1dd662a69f0f28ff5af
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-08-09 22:03:30 +0530

    added route for shipping

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,6 +21,7 @@ import UpdatePassword from './components/User/UpdatePassword';
 import ForgotPassword from './components/User/ForgotPassword';
 import ResetPassword from './components/User/ResetPassword';
 import Cart from './components/Cart/Cart';
+import Shipping from './components/Cart/Shipping'
 
 function App() {
 
@@ -63,6 +64,9 @@ useEffect(() => {
         <Route exact path="/password/forgot" element={<ForgotPassword />} />
         <Route exact path="/password/reset/:token" element={<ResetPassword />} />
         <Route exact path="/cart" element={<Cart />} />
+        <Route path="/shipping" element={<ProtectedRoute />}>
+          <Route exact path="/shipping" element={<Shipping />} />
+        </Route>
       </Routes>
       <Footer />
     </Router>   
