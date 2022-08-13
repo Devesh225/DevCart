5e32e8300cbe96a9114e1422b5d9321997020e0f
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-08-13 19:23:06 +0530

    fixed minor warning bug

--- frontend/src/components/Route/ProtectedRoute.js
+++ frontend/src/components/Route/ProtectedRoute.js
@@ -3,9 +3,9 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
-  const { loading, isAuthenticated, user } = useSelector((state) => state.user);
+  const { loading, isAuthenticated } = useSelector((state) => state.user);
 
-  return !loading && isAuthenticated ? <Outlet /> : <Navigate to="/login" />
+  return !loading && isAuthenticated ? <Outlet {...rest}/> : <Navigate to="/login" />
 };
 
 export default ProtectedRoute;
