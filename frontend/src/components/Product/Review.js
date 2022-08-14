5286c762ad6efef104f98b374dd018c9e71fdd74
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-08-14 23:59:45 +0530

    created review component for adding reviews

--- frontend/src/components/Product/Review.js
+++ frontend/src/components/Product/Review.js
@@ -1,24 +1,22 @@
 import React from 'react'
-import ReactStars from 'react-rating-stars-component'
+import { Rating } from '@mui/material'
 import './Review.css';
 
 const Review = ({ review }) => {
 
-    const options = {
-        edit: false, // CANNOT EDIT THE STARS
-        color: "#393E46",
-        activeColor: "#ffd700", 
-        value: review.rating,
-        isHalf: true, // IF IT IS NOT GIVEN, RATINGS WILL BE SHOWN IN INTEGER VALUES, SO 3.5 WOULD BE SHOWN AS 3
-        size: window.innerWidth < 600 ? 15 : 20,
-    }
+  const options = {
+    readOnly: true, // CANNOT EDIT THE STARS
+    value: review.rating,
+    precision: 0.5, // IF IT IS NOT GIVEN, RATINGS WILL BE SHOWN IN INTEGER VALUES, SO 3.5 WOULD BE SHOWN AS 3
+    size: "medium",
+  }
 
 
   return (
     <div className='reviewCard'>
-        <img src={review.profilePicture} alt="User" />
+        <img className="reviewImage" src={review.profilePicture} alt="User" />
         <p>{review.name}</p>
-        <ReactStars {...options} />
+        <Rating {...options} />
         <span>{review.comment}</span>
     </div>
   )
