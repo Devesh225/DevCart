efe49e073e1223a3282227b3e729960851e97689
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-08-14 23:59:27 +0530

    minor bug of outlet return resolved

--- frontend/src/components/Route/ProtectedRoute.js
+++ frontend/src/components/Route/ProtectedRoute.js
@@ -2,10 +2,10 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoute = ({ component: Component, ...rest }) => {
+const ProtectedRoute = () => {
   const { loading, isAuthenticated } = useSelector((state) => state.user);
 
-  return !loading && isAuthenticated ? <Outlet {...rest}/> : <Navigate to="/login" />
+  return !loading && isAuthenticated ? <Outlet/> : <Navigate to="/login" />
 };
 
 export default ProtectedRoute;
