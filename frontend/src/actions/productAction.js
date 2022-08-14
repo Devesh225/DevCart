d43d427d1e471b1fc306bcb2c7c07c60ec16f5f5
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-08-15 00:02:21 +0530

    created functionality of adding new reviews, implemented

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_FAILURE, CLEAR_ERRORS, PRODUCT_REQUEST, PRODUCT_SUCCESS, PRODUCT_FAILURE } from '../constants/productConstants';
+import { ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_FAILURE, CLEAR_ERRORS, PRODUCT_REQUEST, PRODUCT_SUCCESS, PRODUCT_FAILURE, NEW_REVIEW_REQUEST, NEW_REVIEW_SUCCESS, NEW_REVIEW_FAILURE } from '../constants/productConstants';
 
 export const getAllProducts = (keyword="", currentPage=1, price=[0, 500000], category="", rating=0) => async (dispatch) => {
     try {
@@ -47,6 +47,32 @@ export const getProduct = (id) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: PRODUCT_FAILURE,
+            payload: error.response.data.message
+        });
+    }
+}
+
+export const addNewReview = (reviewData) => async (dispatch) => {
+    try {
+        dispatch({
+            type: NEW_REVIEW_REQUEST,
+        });
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+        
+        const data = await axios.put('/api/review', reviewData, config);
+
+        dispatch({
+            type: NEW_REVIEW_SUCCESS,
+            payload: data.success
+        })
+    } catch (error) {
+        dispatch({
+            type: NEW_REVIEW_FAILURE,
             payload: error.response.data.message
         });
     }
