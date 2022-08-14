668fbd28daf827226544f58b5e8a6d1edc116ce8
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-08-15 00:06:44 +0530

    ViewMyOrders component created and implemented to view user's orders

--- frontend/src/components/Order/ViewMyOrders.js
+++ frontend/src/components/Order/ViewMyOrders.js
@@ -12,54 +12,54 @@ import LaunchIcon from "@mui/icons-material/Launch";
 
 const MyOrders = () => {
   const dispatch = useDispatch();
-  const alert = useAlert();       
+  const alert = useAlert();
   const { loading, error, orders } = useSelector((state) => state.myOrders);
   const { user } = useSelector((state) => state.user);
 
   const columns = [
-    { 
-        field: "id", headerName: "Order ID", minWidth: 300, flex: 1 
+    {
+      field: "id", headerName: "Order ID", minWidth: 300, flex: 0.5
     },
     {
-        field: "status",
-        headerName: "Status",
-        minWidth: 150,
-        flex: 0.5,
-        cellClassName: (params) => {
-            return params.getValue(params.id, "status") === "Delivered"
-            ? "greenColor"
-            : "redColor";
-        },
+      field: "status",
+      headerName: "Status",
+      minWidth: 150,
+      flex: 0.3,
+      cellClassName: (params) => {
+        return params.getValue(params.id, "status") === "Delivered"
+          ? "greenColor"
+          : "redColor";
+      },
     },
     {
-        field: "itemsQty",
-        headerName: "Items Qty",
-        type: "number",
-        minWidth: 150,
-        flex: 0.3,
+      field: "itemsQty",
+      headerName: "Quantity",
+      type: "number",
+      minWidth: 150,
+      flex: 0.3,
     },
     {
-        field: "amount",
-        headerName: "Amount",
-        type: "number",
-        minWidth: 270,
-        flex: 0.5,
+      field: "amount",
+      headerName: "Amount",
+      type: "number",
+      minWidth: 270,
+      flex: 0.5,
     },
 
     {
-        field: "actions",
-        flex: 0.3,
-        headerName: "Actions",
-        minWidth: 150,
-        type: "number",
-        sortable: false,
-        renderCell: (params) => {
-            return (
-            <Link to={`/order/${params.getValue(params.id, "id")}`}>
-                <LaunchIcon />
-            </Link>
-            );
-        },
+      field: "actions",
+      flex: 0.3,
+      headerName: "Actions",
+      minWidth: 150,
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <Link to={`/order/${params.getValue(params.id, "id")}`}>
+            <LaunchIcon />
+          </Link>
+        );
+      },
     },
   ];
 
@@ -67,31 +67,32 @@ const MyOrders = () => {
 
   orders &&
     orders.forEach((item) => {
-        rows.push({
-            itemsQty: item.orderItems.length,
-            id: item._id,
-            status: item.orderStatus,
-            amount: item.totalPrice,
-        });
+      rows.push({
+        itemsQty: item.orderItems.length,
+        id: item._id,
+        status: item.orderStatus,
+        amount: item.totalPrice,
+      });
     });
 
   useEffect(() => {
-        if (error) {
-        alert.error(error);
-        dispatch(clearErrors());
-        }
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
 
-        dispatch(viewMyOrders());
+    dispatch(viewMyOrders());
   }, [dispatch, alert, error]);
 
   return (
     <Fragment>
-      <MetaData title={'Orders'} />
+      <MetaData title={'My Orders'} />
 
       {loading ? (
         <Loading />
       ) : (
         <div className="myOrdersPage">
+          <Typography id="myOrdersHeading">{user.name}'s Orders</Typography>
           <DataGrid
             rows={rows}
             columns={columns}
@@ -100,8 +101,6 @@ const MyOrders = () => {
             className="myOrdersTable"
             autoHeight
           />
-
-          <Typography id="myOrdersHeading">{user.name}'s Orders</Typography>
         </div>
       )}
     </Fragment>
