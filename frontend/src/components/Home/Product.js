08f0800afcc1118582d7623139cb6befabfec18b
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-08-15 00:04:17 +0530

    minor changes in Product component, replaced reactrating with MUI Rating component

--- frontend/src/components/Home/Product.js
+++ frontend/src/components/Home/Product.js
@@ -1,25 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import ReactStars from 'react-rating-stars-component';
+import { Rating } from '@mui/material';
 import './Product.css';
 
 const Product = ({ product }) => {
 
   const options = {
-    edit: false, // CANNOT EDIT THE STARS
-    color: "#393E46",
-    activeColor: "#00ADB5",
+    readOnly: true, // CANNOT EDIT THE STARS
     value: product.rating,
-    isHalf: true, // IF IT IS NOT GIVEN, RATINGS WILL BE SHOWN IN INTEGER VALUES, SO 3.5 WOULD BE SHOWN AS 3
-    size: window.innerWidth < 600 ? 10 : 20
-  }
+    precision: 0.5, // IF IT IS NOT GIVEN, RATINGS WILL BE SHOWN IN INTEGER VALUES, SO 3.5 WOULD BE SHOWN AS 3
+    size: "medium",
+}
 
   return (
     <Link className="productCard" to={`/product/${product._id}`}>
         <img src={product.images[0].url} alt={product.name} />
         <p>{product.name}</p>
         <div>
-            <ReactStars {...options} /> <span>({product.numberOfReviews} Reviews)</span>
+            <Rating {...options} /> <span className="review_span">({product.numberOfReviews} Reviews)</span>
         </div>
         <span>{product.price}</span>
     </Link>
