60c757e34e849481245f712096a5a561257f29fa
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-08-20 15:47:25 +0530

    created reducer for all products admin functionality

--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -1,11 +1,12 @@
 // We need to pass a state and an action, state is an empty list of products.
 // Importing Constants 
-import { ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_FAILURE, CLEAR_ERRORS, PRODUCT_REQUEST, PRODUCT_SUCCESS, PRODUCT_FAILURE, NEW_REVIEW_REQUEST, NEW_REVIEW_SUCCESS, NEW_REVIEW_FAILURE, NEW_REVIEW_RESET } from '../constants/productConstants';
+import { ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_FAILURE, CLEAR_ERRORS, PRODUCT_REQUEST, PRODUCT_SUCCESS, PRODUCT_FAILURE, NEW_REVIEW_REQUEST, NEW_REVIEW_SUCCESS, NEW_REVIEW_FAILURE, NEW_REVIEW_RESET, ADMIN_ALL_PRODUCT_REQUEST, ADMIN_ALL_PRODUCT_FAILURE, ADMIN_ALL_PRODUCT_SUCCESS } from '../constants/productConstants';
 
 
 export const allProductsReducer = (state = {products: []}, action) => {
     switch (action.type) {
         case ALL_PRODUCT_REQUEST:
+        case ADMIN_ALL_PRODUCT_REQUEST:
             return {
                 loading: true,
                 products: []
@@ -20,7 +21,14 @@ export const allProductsReducer = (state = {products: []}, action) => {
                 filteredProductsCount: action.payload.data.filteredProductsCount
             }
         
+        case ADMIN_ALL_PRODUCT_SUCCESS:
+            return {
+                loading: false,
+                products: action.payload
+            }
+
         case ALL_PRODUCT_FAILURE:
+        case ADMIN_ALL_PRODUCT_FAILURE:
             return {
                 loading: false,
                 error: action.payload
