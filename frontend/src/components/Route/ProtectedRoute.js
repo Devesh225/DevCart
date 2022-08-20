c9ea9988588530a4b9a490ce14ddda72f878f932
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-08-20 15:46:47 +0530

    resolved minor bugs of routing

--- frontend/src/components/Route/ProtectedRoute.js
+++ frontend/src/components/Route/ProtectedRoute.js
@@ -2,10 +2,15 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoute = () => {
-  const { loading, isAuthenticated } = useSelector((state) => state.user);
-
-  return !loading && isAuthenticated ? <Outlet/> : <Navigate to="/login" />
+const ProtectedRoute = ({ isAdmin }) => {
+  const { loading, isAuthenticated, user } = useSelector((state) => state.user);
+  if(loading === false && isAuthenticated === true) {
+    return <Outlet/>
+  }
+  if(loading === false && isAuthenticated === true && isAdmin === true && user.role !== "admin") {
+    return <Navigate to="/login" />
+  }
+  
 };
 
 export default ProtectedRoute;
