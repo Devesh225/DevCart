71e2dd53bcb64fe2aade6a2a0d13e5716432d40a
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-08-24 21:32:22 +0530

    created reducer to get all users, user details, update and delete user

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -1,4 +1,4 @@
-import { CLEAR_ERRORS, FORGOT_PASSWORD_FAILURE, FORGOT_PASSWORD_REQUEST, FORGOT_PASSWORD_SUCCESS, LOAD_LOGGED_IN_USER_FAILURE, LOAD_LOGGED_IN_USER_REQUEST, LOAD_LOGGED_IN_USER_SUCCESS, LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT_FAILURE, LOGOUT_SUCCESS, REGISTER_FAILURE, REGISTER_REQUEST, REGISTER_SUCCESS, RESET_PASSWORD_FAILURE, RESET_PASSWORD_REQUEST, RESET_PASSWORD_SUCCESS, UPDATE_PASSWORD_FAILURE, UPDATE_PASSWORD_REQUEST, UPDATE_PASSWORD_RESET, UPDATE_PASSWORD_SUCCESS, UPDATE_PROFILE_FAILURE, UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_RESET, UPDATE_PROFILE_SUCCESS } from "../constants/userConstants";
+import { ADMIN_ALL_USER_FAILURE, ADMIN_ALL_USER_REQUEST, ADMIN_ALL_USER_SUCCESS, ADMIN_DELETE_USER_FAILURE, ADMIN_DELETE_USER_REQUEST, ADMIN_DELETE_USER_RESET, ADMIN_DELETE_USER_SUCCESS, ADMIN_UPDATE_USER_FAILURE, ADMIN_UPDATE_USER_REQUEST, ADMIN_UPDATE_USER_RESET, ADMIN_UPDATE_USER_SUCCESS, ADMIN_USER_FAILURE, ADMIN_USER_REQUEST, ADMIN_USER_SUCCESS, CLEAR_ERRORS, FORGOT_PASSWORD_FAILURE, FORGOT_PASSWORD_REQUEST, FORGOT_PASSWORD_SUCCESS, LOAD_LOGGED_IN_USER_FAILURE, LOAD_LOGGED_IN_USER_REQUEST, LOAD_LOGGED_IN_USER_SUCCESS, LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT_FAILURE, LOGOUT_SUCCESS, REGISTER_FAILURE, REGISTER_REQUEST, REGISTER_SUCCESS, RESET_PASSWORD_FAILURE, RESET_PASSWORD_REQUEST, RESET_PASSWORD_SUCCESS, UPDATE_PASSWORD_FAILURE, UPDATE_PASSWORD_REQUEST, UPDATE_PASSWORD_RESET, UPDATE_PASSWORD_SUCCESS, UPDATE_PROFILE_FAILURE, UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_RESET, UPDATE_PROFILE_SUCCESS } from "../constants/userConstants";
 
 export const userReducer = (state = {user: {}}, action) => {
     switch (action.type) {
@@ -60,30 +60,53 @@ export const updateProfileReducer = (state = {}, action) => {
     switch (action.type) {
         case UPDATE_PROFILE_REQUEST:
         case UPDATE_PASSWORD_REQUEST:
+        case ADMIN_UPDATE_USER_REQUEST:
+        case ADMIN_DELETE_USER_REQUEST:
             return {
                 ...state,
                 loading: true,
             };
         case UPDATE_PROFILE_SUCCESS: 
         case UPDATE_PASSWORD_SUCCESS:
+        case ADMIN_UPDATE_USER_SUCCESS:
             return {
                 ...state,
                 loading: false,
                 isUpdated: action.payload
             }
+
+        case ADMIN_DELETE_USER_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                isDeleted: action.payload.success,
+                message: action.payload.message
+            }
+
         case UPDATE_PROFILE_FAILURE:
         case UPDATE_PASSWORD_FAILURE:
+        case ADMIN_UPDATE_USER_FAILURE:
+        case ADMIN_DELETE_USER_FAILURE:
             return {
                 ...state,
                 loading: false,
                 error: action.payload
             }
+
         case UPDATE_PROFILE_RESET:
         case UPDATE_PASSWORD_RESET:
+        case ADMIN_UPDATE_USER_RESET:
             return {
                 ...state,
                 isUpdated: false
             }
+
+        case ADMIN_DELETE_USER_RESET:
+            return {
+                ...state,
+                isDeleted: false
+            }
+
         case CLEAR_ERRORS:
                 return {
                     ...state,
@@ -131,3 +154,62 @@ export const forgotPasswordReducer = (state = {}, action) => {
                 return state;
     }
 }
+
+export const adminAllUsersReducer = (state = {users: []}, action) => {
+    switch (action.type) {
+        case ADMIN_ALL_USER_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            }
+        case ADMIN_ALL_USER_SUCCESS: 
+            return {
+                ...state,
+                loading: false,
+                users: action.payload
+            }
+        case ADMIN_ALL_USER_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                success: action.payload
+            }
+        case CLEAR_ERRORS:
+                return {
+                    ...state,
+                    error: null
+                }
+        default:
+                return state;
+    }
+}
+
+export const adminUserReducer = (state = {user: {}}, action) => {
+    switch (action.type) {
+        case ADMIN_USER_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            };
+        case ADMIN_USER_SUCCESS: 
+            return {
+                ...state,
+                loading: false,
+                user: action.payload
+            }
+        case ADMIN_USER_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                success: action.payload
+            }
+        case CLEAR_ERRORS:
+                return {
+                    ...state,
+                    error: null
+                }
+        default:
+                return state;
+    }
+}
+
