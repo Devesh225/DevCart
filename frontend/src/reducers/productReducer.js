91fb733a3dcdf2f9255c238e68728b8d6ce7ac26
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-08-24 21:38:21 +0530

    created product reducer to view products, update and delete products, when role is admin

--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -1,6 +1,6 @@
 // We need to pass a state and an action, state is an empty list of products.
 // Importing Constants 
-import { ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_FAILURE, CLEAR_ERRORS, PRODUCT_REQUEST, PRODUCT_SUCCESS, PRODUCT_FAILURE, NEW_REVIEW_REQUEST, NEW_REVIEW_SUCCESS, NEW_REVIEW_FAILURE, NEW_REVIEW_RESET, ADMIN_ALL_PRODUCT_REQUEST, ADMIN_ALL_PRODUCT_FAILURE, ADMIN_ALL_PRODUCT_SUCCESS, ADMIN_NEW_PRODUCT_REQUEST, ADMIN_NEW_PRODUCT_SUCCESS, ADMIN_NEW_PRODUCT_RESET, ADMIN_NEW_PRODUCT_FAILURE } from '../constants/productConstants';
+import { ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_FAILURE, CLEAR_ERRORS, PRODUCT_REQUEST, PRODUCT_SUCCESS, PRODUCT_FAILURE, NEW_REVIEW_REQUEST, NEW_REVIEW_SUCCESS, NEW_REVIEW_FAILURE, NEW_REVIEW_RESET, ADMIN_ALL_PRODUCT_REQUEST, ADMIN_ALL_PRODUCT_FAILURE, ADMIN_ALL_PRODUCT_SUCCESS, ADMIN_NEW_PRODUCT_REQUEST, ADMIN_NEW_PRODUCT_SUCCESS, ADMIN_NEW_PRODUCT_RESET, ADMIN_NEW_PRODUCT_FAILURE, ADMIN_DELETE_PRODUCT_REQUEST, ADMIN_DELETE_PRODUCT_SUCCESS, ADMIN_DELETE_PRODUCT_RESET, ADMIN_DELETE_PRODUCT_FAILURE, ADMIN_UPDATE_PRODUCT_REQUEST, ADMIN_UPDATE_PRODUCT_FAILURE, ADMIN_UPDATE_PRODUCT_SUCCESS, ADMIN_UPDATE_PRODUCT_RESET } from '../constants/productConstants';
 
 
 export const allProductsReducer = (state = {products: []}, action) => {
@@ -135,6 +135,59 @@ export const newProductAdminReducer = (state = {product: {}}, action) => {
             }
         
         case ADMIN_NEW_PRODUCT_FAILURE:
+            return {
+                loading: false,
+                error: action.payload
+            }
+
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+    
+        default:
+            return state;
+    }
+}
+
+export const productAdminReducer = (state = {}, action) => {
+    switch (action.type) {
+        case ADMIN_DELETE_PRODUCT_REQUEST:
+        case ADMIN_UPDATE_PRODUCT_REQUEST:
+            return {
+                loading: true,
+                ...state
+            }
+
+        case ADMIN_DELETE_PRODUCT_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                isDeleted: action.payload
+            }
+
+        case ADMIN_UPDATE_PRODUCT_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                isUpdated: action.payload
+            }
+
+        case ADMIN_DELETE_PRODUCT_RESET:
+            return {
+                ...state,
+                isDeleted: false,
+            }
+
+        case ADMIN_UPDATE_PRODUCT_RESET:
+            return {
+                ...state,
+                isUpdated: false,
+            }
+        
+        case ADMIN_DELETE_PRODUCT_FAILURE:
+        case ADMIN_UPDATE_PRODUCT_FAILURE:
             return {
                 loading: false,
                 error: action.payload
