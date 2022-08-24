b7b1af238d571131952d0035b9e834b55751e228
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-08-24 21:36:28 +0530

    implemented functionality to view all products, update and delete products

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_FAILURE, CLEAR_ERRORS, PRODUCT_REQUEST, PRODUCT_SUCCESS, PRODUCT_FAILURE, NEW_REVIEW_REQUEST, NEW_REVIEW_SUCCESS, NEW_REVIEW_FAILURE, ADMIN_ALL_PRODUCT_REQUEST, ADMIN_ALL_PRODUCT_SUCCESS, ADMIN_ALL_PRODUCT_FAILURE, ADMIN_NEW_PRODUCT_REQUEST, ADMIN_NEW_PRODUCT_SUCCESS, ADMIN_NEW_PRODUCT_FAILURE } from '../constants/productConstants';
+import { ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_FAILURE, CLEAR_ERRORS, PRODUCT_REQUEST, PRODUCT_SUCCESS, PRODUCT_FAILURE, NEW_REVIEW_REQUEST, NEW_REVIEW_SUCCESS, NEW_REVIEW_FAILURE, ADMIN_ALL_PRODUCT_REQUEST, ADMIN_ALL_PRODUCT_SUCCESS, ADMIN_ALL_PRODUCT_FAILURE, ADMIN_NEW_PRODUCT_REQUEST, ADMIN_NEW_PRODUCT_SUCCESS, ADMIN_NEW_PRODUCT_FAILURE, ADMIN_DELETE_PRODUCT_REQUEST, ADMIN_DELETE_PRODUCT_SUCCESS, ADMIN_DELETE_PRODUCT_FAILURE, ADMIN_UPDATE_PRODUCT_REQUEST, ADMIN_UPDATE_PRODUCT_SUCCESS, ADMIN_UPDATE_PRODUCT_FAILURE } from '../constants/productConstants';
 
 export const getAllProducts = (keyword="", currentPage=1, price=[0, 500000], category="", rating=0) => async (dispatch) => {
     try {
@@ -110,7 +110,7 @@ export const createNewProduct = (productData) => async (dispatch) => {
             }
         }
         
-        const data = await axios.put('/api/admin/product/new', productData, config);
+        const { data } = await axios.post('/api/admin/product/new', productData, config);
 
         dispatch({
             type: ADMIN_NEW_PRODUCT_SUCCESS,
@@ -119,6 +119,46 @@ export const createNewProduct = (productData) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: ADMIN_NEW_PRODUCT_FAILURE,
+            payload: error.response.data.message
+        });
+    }
+}
+
+export const deleteProduct = (id) => async (dispatch) => {
+    try {
+        dispatch({
+            type: ADMIN_DELETE_PRODUCT_REQUEST,
+        });
+        
+        const { data } = await axios.delete(`/api/admin/product/${id}`);
+
+        dispatch({
+            type: ADMIN_DELETE_PRODUCT_SUCCESS,
+            payload: data.success
+        })
+    } catch (error) {
+        dispatch({
+            type: ADMIN_DELETE_PRODUCT_FAILURE,
+            payload: error.response.data.message
+        });
+    }
+}
+
+export const updateProduct = (id, productData) => async (dispatch) => {
+    try {
+        dispatch({
+            type: ADMIN_UPDATE_PRODUCT_REQUEST,
+        });
+        
+        const data = await axios.put(`/api/admin/product/${id}`);
+
+        dispatch({
+            type: ADMIN_UPDATE_PRODUCT_SUCCESS,
+            payload: data.success
+        })
+    } catch (error) {
+        dispatch({
+            type: ADMIN_UPDATE_PRODUCT_FAILURE,
             payload: error.response.data.message
         });
     }
