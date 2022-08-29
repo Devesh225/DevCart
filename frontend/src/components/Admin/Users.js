52e7c58e5ea1a55c0d4811b0d7bc08314f33540c
Devesh Tulshyan <deveshtulshyan@gmail.com>
2022-08-29 19:47:29 +0530

    User Details Component For Admin

---
+++ frontend/src/components/Admin/Users.js
@@ -0,0 +1,138 @@
+import React, { Fragment, useEffect } from "react";
+import { DataGrid } from "@material-ui/data-grid";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { useAlert } from "react-alert";
+import { Button } from "@mui/material";
+import MetaData from "../layout/MetaData";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SideBar from "./Sidebar";
+import { adminViewAllUsers, clearErrors, deleteUser } from "../../actions/userAction";
+import { ADMIN_DELETE_USER_RESET } from "../../constants/userConstants";
+
+const Users = () => {
+  const dispatch = useDispatch();
+  const alert = useAlert();
+  const navigate = useNavigate();
+
+  const { error, users } = useSelector((state) => state.allUsers);
+
+  const { error: deleteError, isDeleted, message} = useSelector((state) => state.profile);
+
+  const deleteUserHandler = (id) => {
+    dispatch(deleteUser(id));
+  };
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+
+    if (deleteError) {
+      alert.error(deleteError);
+      dispatch(clearErrors());
+    }
+
+    if (isDeleted) {
+      alert.success(message);
+      navigate("/admin/users");
+      dispatch({ type: ADMIN_DELETE_USER_RESET });
+    }
+
+    dispatch(adminViewAllUsers());
+  }, [dispatch, alert, error, deleteError, navigate, isDeleted, message]);
+
+  const columns = [
+    { field: "id", headerName: "User ID", minWidth: 180, flex: 0.8 },
+
+    {
+      field: "email",
+      headerName: "Email",
+      minWidth: 200,
+      flex: 1,
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      minWidth: 150,
+      flex: 0.5,
+    },
+
+    {
+      field: "role",
+      headerName: "Role",
+      type: "number",
+      minWidth: 150,
+      flex: 0.3,
+      cellClassName: (params) => {
+        return params.getValue(params.id, "role") === "admin"
+          ? "greenColor"
+          : "redColor";
+      },
+    },
+
+    {
+      field: "actions",
+      flex: 0.3,
+      headerName: "Actions",
+      minWidth: 150,
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <Fragment>
+            <Button><Link to={`/admin/user/${params.getValue(params.id, "id")}`}>
+              <EditIcon />
+            </Link></Button>
+
+            <Button
+              onClick={() =>
+                deleteUserHandler(params.getValue(params.id, "id"))
+              }
+            >
+              <DeleteIcon />
+            </Button>
+          </Fragment>
+        );
+      },
+    },
+  ];
+
+  const rows = [];
+
+  users &&
+    users.forEach((item) => {
+      rows.push({
+        id: item._id,
+        role: item.role,
+        email: item.email,
+        name: item.name,
+      });
+    });
+
+  return (
+    <Fragment>
+      <MetaData title={`View All Users`} />
+
+      <div className="dashboard">
+        <SideBar />
+        <div className="productListContainer">
+          <h1 id="productListHeading">ALL USERS</h1>
+
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={25}
+            disableSelectionOnClick
+            className="productListTable"
+            autoHeight
+          />
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Users;
